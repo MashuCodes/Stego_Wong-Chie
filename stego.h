54e063aeafd57127ab7719efd44076f70a687b8f
MashuCodes <mawong0923@gmail.com>
2024-11-19 17:17:12 +0900

    Update stego.h

--- stego.h
+++ stego.h
@@ -1,1 +1,11 @@
 //stego.h contains declarations of constants, structs, functions, etc., common to both programs
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// Function to parse header, checks and returns 1 if header begins with "P6\0" and returns 0 if otherwise
+int parse_header(FILE *);
+
+// Function to read the original file, and write the hidden byte onto the output file
+void write_hidden_byte(FILE *, FILE *, char);
