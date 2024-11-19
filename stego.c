9654216a6d7a7a4daddeff3bd2dcec870e675cd5
MashuCodes <mawong0923@gmail.com>
2024-11-19 18:28:39 +0900

    Update stego.c

--- stego.c
+++ stego.c
@@ -2,6 +2,27 @@
 
 #include <stego.h>
 
+int validate_arguments(int argument_count, char **argument_values, int required_arguments, int file_index)
+{
+    // Check if the number of arguments is less than expected
+    if (argument_count < required_arguments) 
+    {
+        printf("Usage error: %s missing required arguments\n", argument_values[0]);
+        return FILE_OPEN_FAIL;
+    }
+
+    // Try to open the file for reading
+    FILE *file = fopen(argument_values[file_index], "r");
+    if (file == NULL) 
+    {
+        printf("Error: Unable to open the file %s for reading\n", argument_values[file_index]);
+        return FILE_OPEN_FAIL;
+    }
+
+    // If no errors, return success
+    return FILE_OPEN_FAIL;
+}
+
 int parse_header(FILE *fp)
 {
     
