f67ef57d9ed8a05b5e380eb7bddb7400646da462
MashuCodes <mawong0923@gmail.com>
2024-11-20 14:52:39 +0900

    Update stego.h

--- stego.h
+++ stego.h
@@ -4,6 +4,32 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+#define FILE_OPEN_SUCCESS 1
+#define FILE_OPEN_FAIL 0
+#define PARSE_HEADER_SUCCESS 1
+#define PARSE_HEADER_FAIL 0
+#define EXIT_SUCCESS 0
+
+
+struct msg 
+{
+    char *data;  // Pointer to the hidden message
+    int length;  // Length of the hidden message
+};
+
+struct ppm 
+{
+    char magic_number[3];
+    char comments[2];
+    int width;
+    int height;
+    int Maxval;
+    struct msg hidden_msg;
+};
+
+// Function to validate command-line arguments and open the file
+int validate_arguments(int argument_count, char** argument_values, int required_arguments, int file_index);
+
 // Function to parse header, checks and returns 1 if header begins with "P6\0" and returns 0 if otherwise
 int parse_header(FILE *);
 
@@ -15,3 +41,6 @@ unsigned int read_length(FILE *);
 
 // Function to read a byte and returns it as a single character
 unsigned int read_byte(FILE *);
+
+// Function that parses and validates the header before processing the rest of the PPM file by checking for P6
+int readHeader(struct ppm *pi, FILE *fp);
